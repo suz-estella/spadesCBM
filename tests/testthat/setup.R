4f7887e8b163d52d29333feabe1c377175562a19
Susan Murray <murray.e.susan@gmail.com>
2025-05-13 15:56:49 -0700

    tests: install quickPlot from Github

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -6,7 +6,7 @@ if (!testthat::is_testing()){
 
 # 2025-05: Install latest quickPlot from Github required by LandR
 ## Otherwise SpaDES.core will install and load an older version from CRAN first
-Require::Install("PredictiveEcology/quickPlot@development")
+Require::Install("PredictiveEcology/quickPlot@development (>= 1.0.2.9001)")
 
 # Source work in progress SpaDES module testing functions
 suppressPackageStartupMessages(library(SpaDES.core))
